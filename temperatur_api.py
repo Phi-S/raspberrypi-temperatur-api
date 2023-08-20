48b7e06d1df0799712e3a3012511a87de947009e
Phi-S <926151+Phi-S@users.noreply.github.com>
2023-08-20 03:14:56 +0200

    Replaced temperature and humidity endpoints with a single one. Added retries

--- temperatur_api.py
+++ temperatur_api.py
@@ -5,29 +5,32 @@ import time
 import board
 import adafruit_dht
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 
 port = 8001
 
 dhtDevice = adafruit_dht.DHT22(board.D4)
-dhtDevice.humidity
-dhtDevice.temperature
-
 app = FastAPI()
 
-@app.get("/humidity")
-def humidity_root():
-    humidity = dhtDevice.humidity
-    if humidity is None:
-        raise HTTPException(status_code=500, detail="Failed to get valid humidity")
-    return humidity
 
-@app.get("/temperature")
+@app.get("/")
 def temperature_root():
-    temperature = dhtDevice.temperature
-    if temperature is None:
-        raise HTTPException(status_code=500, detail="Failed to get valid temperature")
-    return temperature
+    for i in range(10):
+        try:
+            temperature = dhtDevice.temperature
+            humidity = dhtDevice.humidity
+            if (isinstance(temperature, int) or isinstance(temperature, float)) and (
+                    isinstance(humidity, int) or isinstance(humidity, float)):
+                return {"temperature": temperature, "humidity": humidity}
+            else:
+                print("Not an int or float. retry")
+                continue
+        except Exception:
+            print("retry")
+            continue
+    else:
+        return Response(status_code=500)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=port)
